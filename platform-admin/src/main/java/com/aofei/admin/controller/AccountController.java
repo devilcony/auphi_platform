6925990a39c7a1e3c777c98f63bce2fc9841b7b4
Tony <longyli@163.com>
2018-09-26 17:54:39 +0800

    开发

--- platform-admin/src/main/java/com/aofei/admin/controller/AccountController.java
+++ platform-admin/src/main/java/com/aofei/admin/controller/AccountController.java
@@ -1,13 +1,12 @@
 package com.aofei.admin.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.aofei.admin.authorization.Token;
 import com.aofei.admin.authorization.jwt.JwtConfig;
 import com.aofei.admin.authorization.jwt.JwtTokenBuilder;
-import com.aofei.authorization.manager.TokenManager;
 import com.aofei.base.common.UserUtil;
 import com.aofei.base.controller.BaseController;
 import com.aofei.base.exception.ApplicationException;
-import com.aofei.base.exception.StatusCode;
 import com.aofei.base.model.response.CurrentUserResponse;
 import com.aofei.base.model.response.Response;
 import com.aofei.sys.exception.SystemError;
@@ -16,12 +15,12 @@ import com.aofei.sys.model.response.UserResponse;
 import com.aofei.sys.service.IUserService;
 import com.aofei.utils.BeanCopier;
 import com.aofei.utils.StringUtils;
-import io.jsonwebtoken.Claims;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -47,17 +46,13 @@ public class AccountController extends BaseController {
     @Autowired
     private JwtTokenBuilder jwtTokenBuilder;
 
-    @Autowired
-    private TokenManager tokenManager;
-
-
 
     /**
      * 登录
      *
      * @return
      */
-    @ApiOperation(value = "用户登录", notes = "用户登录返回Token", httpMethod = "POST")
+    @ApiOperation(value = "用户登录", notes = "用户登录返回Token,后期访问接口在head中添加Authorization={Token}", httpMethod = "POST")
     @ApiResponses(value = {
             @ApiResponse(code = 200001, message = "invalid username or password"),
             @ApiResponse(code = 200002, message = "captcha error"),
@@ -91,77 +86,67 @@ public class AccountController extends BaseController {
             String subject = JwtTokenBuilder.buildSubject(map);
 
             String accessToken = jwtTokenBuilder.buildToken(subject, jwtConfig.getExpiresSecond(), jwtConfig.getBase64Secret());
-            String refreshToken = jwtTokenBuilder.buildToken(subject, jwtConfig.getRefreshExpiresSecond(), jwtConfig.getRefreshBase64Secret());
+            //String refreshToken = jwtTokenBuilder.buildToken(subject, jwtConfig.getRefreshExpiresSecond(), jwtConfig.getRefreshBase64Secret());
             Token token = new Token();
             token.setAccess_token(accessToken);
-            token.setRefresh_token(refreshToken);
+            //token.setRefresh_token(refreshToken);
             token.setToken_type("bearer");
             token.setExpires_in(jwtConfig.getExpiresSecond());
 
             //存储到redis
-            tokenManager.createRelationship(user.getUsername(), accessToken);
+            //tokenManager.createRelationship(user.getUsername(), accessToken);
 
             return Response.ok(token);
         } else {
             throw new ApplicationException(SystemError.LOGIN_FAILED.getCode(), SystemError.LOGIN_FAILED.getMessage());
         }
-
     }
 
+
     /**
-     * 刷新token，获取新的token
+     * 登录
      *
-     * @param refresh_token
      * @return
-     * @throws Exception
      */
-    @ApiOperation(value = "刷新token，获取新的token", notes = "刷新token，获取新的token", httpMethod = "GET")
-    @RequestMapping(value = "/refresh_token/{refresh_token}", method = RequestMethod.GET)
+    @ApiIgnore
+    @RequestMapping(value = "/client/login", method = RequestMethod.POST)
     @ResponseBody
-    public Response<Token> refreshToken(@PathVariable String refresh_token) throws Exception {
+    public Response<JSONObject> login(
+           @RequestParam(value = "username") String username,
+           @RequestParam(value = "password") String password) throws Exception {
 
-        //验证token
-        Claims claims = jwtTokenBuilder.decodeToken(refresh_token, jwtConfig.getRefreshBase64Secret());
-        if (claims != null) {
-            //如果token验证成功，返回新的token
-            String subject = claims.getSubject();//用户信息
-            String accessToken = jwtTokenBuilder.buildToken(subject, jwtConfig.getExpiresSecond(), jwtConfig.getBase64Secret());
-            String refreshToken = jwtTokenBuilder.buildToken(subject, jwtConfig.getRefreshExpiresSecond(), jwtConfig.getRefreshBase64Secret());
-
-            Token token = new Token();
-            token.setAccess_token(accessToken);
-            token.setRefresh_token(refreshToken);
-            token.setToken_type("bearer");
-            token.setExpires_in(jwtConfig.getExpiresSecond());
-
-            Map<String,String> map = JwtTokenBuilder.decodeSubject(subject);
-            //存储到redis
-            tokenManager.createRelationship(map.get("username"), accessToken);
-
-            return Response.ok(token);
-        } else {
-            throw new ApplicationException(StatusCode.UNAUTHORIZED.getCode(), "invalid refresh token");
-        }
-    }
-
-    /**
-     * 注销
-     *
-     * @return
-     */
-    @ApiOperation(value = "注销登录", notes = "注销登录", httpMethod = "POST")
-    @RequestMapping(value = "/logout", method = RequestMethod.POST)
-    public Integer logout(@RequestHeader(name = "Authorization") String token) {
-        //TODO 操蛋的JWT不能从服务端destroy token， logout目前只能在客户端的cookie 或 localStorage/sessionStorage  remove token
-        //TODO 准备用jwt生成永久的token，再结合redis来实现Logout。具体是把token的生命周期交给redis来管理，jwt只负责生成token
+        //验证码校验
+        UserResponse user = null;
         try {
-            //多端登录，会有多个同一用户名但token不一样的键值对在redis中存在，所以只能通过token删除
-           // tokenManager.delRelationshipByKey(user.getUsername());
-            tokenManager.delRelationshipByToken(token);//注销成功
-            return 1;
-        } catch (Exception e) {
-            return -1;
+            //用户名密码验证
+            user = userService.auth(username, password);
+        }catch (ApplicationException e){
+            throw new ApplicationException(SystemError.LOGIN_FAILED.getCode(), SystemError.LOGIN_FAILED.getMessage());
+        }
+
+        if(user != null) {
+            UserUtil.setSessionUser(BeanCopier.copy(user, CurrentUserResponse.class));
+            String host = StringUtils.getRemoteAddr();
+            UserRequest userRequest = new UserRequest(user.getUserId());
+            userRequest.setLoginIp(host);
+            userRequest.setLoginTime(new Date());
+            userService.updateLogin(userRequest);
+
+            JSONObject object = new JSONObject();
+            object.put("user",user);
+
+
+
+            return Response.ok(object);
+
+
+
+
+        } else {
+            throw new ApplicationException(SystemError.LOGIN_FAILED.getCode(), SystemError.LOGIN_FAILED.getMessage());
         }
     }
+
+
 
 }
--- platform-admin/src/main/java/com/aofei/admin/init/SystemInitBean.java
+++ platform-admin/src/main/java/com/aofei/admin/init/SystemInitBean.java
@@ -1,14 +1,24 @@
 package com.aofei.admin.init;
 
 import com.aofei.kettle.core.PropsUI;
+import com.aofei.sys.model.request.RepositoryRequest;
+import com.aofei.sys.model.response.RepositoryDatabaseResponse;
+import com.aofei.sys.model.response.RepositoryResponse;
+import com.aofei.sys.service.IRepositoryDatabaseService;
+import com.aofei.sys.service.IRepositoryService;
+import com.aofei.sys.utils.DatabaseCodec;
 import org.joda.time.DateTime;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.Props;
+import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.core.logging.KettleLogStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @auther Tony
@@ -17,7 +27,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class SystemInitBean implements InitializingBean {
 
+    @Autowired
+    private IRepositoryService repositoryService;
 
+    @Autowired
+    private IRepositoryDatabaseService repositoryDatabaseService;
 
     private static Logger logger = LoggerFactory.getLogger(SystemInitBean.class);
     /**
@@ -27,17 +41,30 @@ public class SystemInitBean implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         long start = System.currentTimeMillis();
-        StringBuffer startInfo = new StringBuffer(System.getProperty("line.separator"));
+        StringBuffer startInfo = new StringBuffer();
 
-        startInfo.append("********************************************").append(System.getProperty("line.separator"))
-        .append("北京傲飞商智软件有限公司...").append(System.getProperty("line.separator"))
-        .append("********************************************").append(System.getProperty("line.separator"))
-        .append("傲飞数据整合平台...").append(System.getProperty("line.separator"))
-        .append("系统开始启动字典装载程序...").append(System.getProperty("line.separator"))
-        .append("开始加载资源库...").append(System.getProperty("line.separator"));
+        startInfo.append(System.getProperty("line.separator"))
+                .append("********************************************").append(System.getProperty("line.separator"))
+                .append("********北京傲飞商智软件有限公司****************").append(System.getProperty("line.separator"))
+                .append("********傲飞数据整合平台***********************").append(System.getProperty("line.separator"))
+                .append("********系统开始启动字典装载程序****************").append(System.getProperty("line.separator"))
+                .append("********开始加载资源库*************************").append(System.getProperty("line.separator"))
+                .append("********************************************").append(System.getProperty("line.separator"));
         logger.info(startInfo.toString());
         KettleLogStore.init( 5000, 720 );
         KettleEnvironment.init();
+
+        List<RepositoryResponse> repositorys = repositoryService.getRepositorys(new RepositoryRequest());
+
+        for(RepositoryResponse repository : repositorys){
+            RepositoryDatabaseResponse repositoryDatabase =  repositoryDatabaseService.get(repository.getRepositoryConnectionId());
+            DatabaseMeta databaseMeta = DatabaseCodec.decode(repositoryDatabase);
+
+        }
+
+
+
+
         PropsUI.init( "KettleWebConsole", Props.TYPE_PROPERTIES_KITCHEN );
 
 //        KettleDatabaseRepository repository = new KettleDatabaseRepository();
